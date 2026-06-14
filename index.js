5ad0b2f910b8e312f6d19f3c5db3399cac360090
boboyevfirdavs79-rgb <boboyevfirdavs79@gmail.com>
2026-06-14 22:50:35 +0500

    11

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-const express = require('express')
-const cors = require('cors');
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const connectDB = require("./config/db.config");
 const authorRouter = require("./router/author.routes");
 const bookRouter = require("./router/book.routes");
 const errorMiddleware = require("./middleware/error.middleware");
-const authRouter = require('./router/auth.routes');
-require("dotenv").config();
 const cookieParser = require("cookie-parser");
-const citationRouter = require('./router/citation.routes');
-const likeRouter = require('./router/like.routes');
-const path = require("path")
+const CitationRouter = require("./router/citation.routes");
+const logger = require("./utils/logger");
+const YAML = require("yamljs");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
-app.use(cors())
-app.use(cookieParser())
-app.use(express.urlencoded({
-  extended: true
-}))
-
-
+app.use(cors());
+app.use("uploads", express.static(path.join(__dirname, "uploads/images")));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 connectDB();
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads/images")))
-//router
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(YAML.load("./docs/documentation.yml")));
+
 
 app.use(authorRouter);
 app.use(bookRouter);
-app.use(authRouter);
-app.use(citationRouter)
+app.use(CitationRouter);
+
+
 app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log("Server is running at: " + PORT);
+
 });
